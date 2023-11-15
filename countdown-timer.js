a4e555577a62e29bc846117283e6aeee458db634
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-15 11:06:51 +0300

    Update countdown-timer.js

--- countdown-timer.js
+++ countdown-timer.js
@@ -5,9 +5,12 @@ jQuery(document).ready(function ($) {
     var x = setInterval(function () {
         var now = new Date().getTime();
         var distance = countDownDate - now;
+
+        // Calculate minutes and seconds
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+        // Display the countdown
         document.getElementById("countdown-timer").innerHTML = minutes + "m " + seconds + "s ";
 
         if (distance < 0) {
